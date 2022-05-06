cdd26b337c56316e3fdc136bea7b89f7720b318e
MaxLasmar <95002243+MaxLasmar@users.noreply.github.com>
2022-05-06 16:11:24 -0400

    Segunda Entrega Proyecto semestral
    Segunda entrega del proyecto semestral creado por Javiera Lizana y Maximiliano Lasmar. Las conecciones y modificaciones se integraron en Envioexitoso y Unete.

---
+++ proyectosemestral/validaciones.js
@@ -0,0 +1,32 @@
+//let y =$(document)
+//y.ready(IniciarEventos);
+
+//function InicializarEventos(){
+//    let y = $("#boton1");
+//    y.click(ocultarRecuadro);
+//    y = $("#boton2"),
+//    x.click(mostrarRecuadro);
+//}
+
+//function ocultarRecuadro(){
+//    let x = $("#descripcion");
+//    x.fadeOut("slow");
+//}
+
+
+//$("#form1").submit(
+//    function(){
+        //validaciones
+        //si no cumple
+//        event.preventDefault();
+
+//    }
+
+//);
+
+$(document).ready(function(){
+    $("#volver").click(function(){
+        alert("Esta regresando a la pagina de inicio de 'ADOPT A BUDDY' pero su información fue enviada correctamente");
+
+    });
+});
